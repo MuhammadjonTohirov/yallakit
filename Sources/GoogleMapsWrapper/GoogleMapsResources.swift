4522a26342fb58c0f2ae7c2db366d1ac37bc2708
Muhammadjon Tohirov <tokhirov.mukhammadjon@gmail.com>
2025-04-05 12:58:42 +0500

    Brough back bundle

--- Sources/GoogleMapsWrapper/GoogleMapsResources.swift
+++ Sources/GoogleMapsWrapper/GoogleMapsResources.swift
@@ -1,6 +1,12 @@
 import Foundation
 //
-//public struct GoogleMapsResources {
-//    // This empty struct just makes this a valid Swift module
-//    public static let resourceBundle = Bundle.module
-//}
+public class GoogleMapsResources {
+    // This empty struct just makes this a valid Swift module
+    public static let resourceBundle = Bundle.module
+}
+
+extension Bundle {
+    static var module: Bundle {
+        return .init(for: GoogleMapsResources.self)
+    }
+}
