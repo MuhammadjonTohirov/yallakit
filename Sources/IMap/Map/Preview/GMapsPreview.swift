7df8f99d95b56cbe20e6f6fa1167ac6456b9a7e9
Muhammadjon Tohirov <tokhirov.mukhammadjon@gmail.com>
2025-05-01 17:59:46 +0500

    dash line in progress

---
+++ Sources/IMap/Map/Preview/GMapsPreview.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  YallaKit
+//
+//  Created by Muhammadjon Tohirov on 01/05/25.
+//
+
+import Foundation
+import GoogleMaps
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    MapView()
+}
