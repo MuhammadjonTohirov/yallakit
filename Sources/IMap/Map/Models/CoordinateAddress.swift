4c250b9303a087b46e420fa3bf0af6cf1c06d8a8
Muhammadjon Tohirov <tokhirov.mukhammadjon@gmail.com>
2025-05-06 16:35:53 +0500

    FetchCoordinateAddress updated

--- Sources/IMap/Map/Models/CoordinateAddress.swift
+++ Sources/IMap/Map/Models/CoordinateAddress.swift
@@ -12,12 +12,15 @@ public struct CoordinateAddress {
     public var lat: Double
     public var lng: Double
     public var name: String
+    public var distance: Double?
+    public var parent: AddressParent?
+    public var level: String?
     
-    public init(lat: Double, lng: Double, name: String) {
+    public init(id: Int?, lat: Double, lng: Double, name: String) {
         self.lat = lat
         self.lng = lng
         self.name = name
-        self.id = nil
+        self.id = id
     }
     
     init?(with: NetResGetAddress?) {
@@ -29,5 +32,20 @@ public struct CoordinateAddress {
         self.lat = with.lat
         self.lng = with.lng
         self.name = with.name
+        self.distance = with.distance
+        self.parent = with.parent.map({.init(id: $0.id, name: $0.name, lavel: $0.level)})
+        self.level = with.level
+    }
+    
+    public struct AddressParent {
+        public var id: Int
+        public var name: String?
+        public var level: String?
+        
+        public init(id: Int, name: String? = nil, lavel: String? = nil) {
+            self.id = id
+            self.name = name
+            self.level = lavel
+        }
     }
 }
