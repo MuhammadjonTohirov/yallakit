6112b71b37df350c23312e7de992e5c6d3b7da33
Muhammadjon Tohirov <tokhirov.mukhammadjon@gmail.com>
2025-05-09 16:53:50 +0500

    Removed IMap

--- Package.swift
+++ Package.swift
@@ -10,12 +10,7 @@ let package = Package(
         .library(name: "YallaKit", targets: ["YallaKit"]),
         .library(name: "Core", targets: ["Core"]),
         .library(name: "NetworkLayer", targets: ["NetworkLayer"]),
-        .library(name: "IMap", targets: ["IMap"]),
         .library(name: "IldamSDK", targets: ["IldamSDK"])
-    ],
-    dependencies: [
-        // ✅ Google Maps SDK via SPM
-        .package(url: "https://github.com/googlemaps/ios-maps-sdk", from: "9.4.0")
     ],
     targets: [
         .target(
@@ -23,7 +18,6 @@ let package = Package(
             dependencies: [
                 "Core",
                 "NetworkLayer",
-                "IMap",
                 "IldamSDK"
             ]
         ),
@@ -36,46 +30,8 @@ let package = Package(
             dependencies: []
         ),
         .target(
-            name: "IMap",
-            dependencies: [
-                "Core",
-                "NetworkLayer",
-                "GoogleMapsWrapper"
-            ],
-            resources: [
-                // assets
-                .process("Assets.xcassets")
-            ]
-        ),
-        .target(
             name: "IldamSDK",
             dependencies: ["Core", "NetworkLayer"]
-        ),
-        .target(
-            name: "GoogleMapsWrapper",
-            dependencies: [
-                .product(name: "GoogleMaps", package: "ios-maps-sdk")
-            ],
-            linkerSettings: [
-                .linkedFramework("QuartzCore"),
-                .linkedFramework("OpenGLES"),
-                .linkedFramework("CoreText"),
-                .linkedFramework("CoreLocation"),
-                .linkedFramework("CoreGraphics"),
-                .linkedFramework("CoreData"),
-                .linkedFramework("CoreImage"),
-                .linkedFramework("GLKit"),
-                .linkedFramework("ImageIO"),
-                .linkedFramework("Metal"),
-                .linkedFramework("SystemConfiguration"),
-                .linkedFramework("UIKit"),
-                .linkedFramework("Foundation"),
-                .linkedFramework("SwiftUI"),
-                .linkedFramework("CoreTelephony"),
-                .linkedFramework("Accelerate"),
-                .linkedLibrary("c++"),
-                .linkedLibrary("z")
-            ]
         ),
         .testTarget(
             name: "IldamSDKTests",
--- Sources/GoogleMapsWrapper/GoogleMapsResources.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-//
-public class GoogleMapsResources {
-    // This empty struct just makes this a valid Swift module
-}
-
