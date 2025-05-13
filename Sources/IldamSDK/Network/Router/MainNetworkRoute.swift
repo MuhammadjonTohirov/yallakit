94920307c1d44c12ac1db87341906f7c0d22a0a4
Muhammadjon Tohirov <tokhirov.mukhammadjon@gmail.com>
2025-05-13 10:11:04 +0500

    Bugfix

--- Sources/IldamSDK/Network/Router/MainNetworkRoute.swift
+++ Sources/IldamSDK/Network/Router/MainNetworkRoute.swift
@@ -86,6 +86,12 @@ enum MainNetworkRoute: URLRequestProtocol {
             ]
             
             return try? JSONSerialization.data(withJSONObject: dict)
+        case let .getAddress(lat, lng):
+            return [
+                "lat": lat,
+                "lng": lng
+            ].asData
+        
         default:
             return nil
         }
@@ -93,9 +99,9 @@ enum MainNetworkRoute: URLRequestProtocol {
     
     var method: HTTPMethod {
         switch self {
-        case .getAddress, .getMyAddresses, .loadOrderHistory, .orderDetails, .findExecutors:
+        case .getMyAddresses, .loadOrderHistory, .orderDetails, .findExecutors:
             return .get
-        case .getTariffs, .addMyAddress, .getRouteCoordinates, .getNearAddresses, .orderTaxi, .searchAddress:
+        case .getTariffs, .addMyAddress, .getRouteCoordinates, .getNearAddresses, .orderTaxi, .searchAddress, .getAddress:
             return .post
         case .updateMyAddress:
             return .put
@@ -128,3 +134,4 @@ enum MainNetworkRoute: URLRequestProtocol {
     case orderTaxi(req: NetReqOrderTaxi)
     case orderDetails(orderId: Int)
 }
+
