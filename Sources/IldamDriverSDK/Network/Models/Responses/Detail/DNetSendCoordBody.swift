f16bccf1d5e831b33e3380b76ccf0daf3105f647
Muhammadjon Tohirov <tokhirov.mukhammadjon@gmail.com>
2025-06-10 12:01:48 +0500

    Bugfix2

--- Sources/IldamDriverSDK/Network/Models/Responses/Detail/DNetSendCoordBody.swift
+++ Sources/IldamDriverSDK/Network/Models/Responses/Detail/DNetSendCoordBody.swift
@@ -8,29 +8,29 @@
 import SwiftUI
 
 public struct SendCoordsBody: Encodable, Sendable {
-    let coords: SendCoords?
-    let orderCheck: OrderCheck?
+    public let coords: SendCoords?
+    public let orderCheck: OrderCheck?
     
     enum CodingKeys: String, CodingKey {
         case coords
         case orderCheck = "order_ckeck"
     }
     
-    init(coords: SendCoords?, orderCheck: OrderCheck?) {
+    public init(coords: SendCoords?, orderCheck: OrderCheck?) {
         self.coords = coords
         self.orderCheck = orderCheck
     }
 }
 
-struct SendCoords: Encodable, Sendable {
-    let heading: Int
-    let lat: Double
-    let lng: Double
-    let speed: Double
-    let orderStatus: String?
-    let orderId: Int?
-    let online: Bool?
-    let statusTime: String?
+public struct SendCoords: Encodable, Sendable {
+    public let heading: Int
+    public let lat: Double
+    public let lng: Double
+    public let speed: Double
+    public let orderStatus: String?
+    public let orderId: Int?
+    public let online: Bool?
+    public let statusTime: String?
     
     enum CodingKeys: String, CodingKey {
         case heading
@@ -42,25 +42,36 @@ struct SendCoords: Encodable, Sendable {
         case online
         case statusTime = "status_time"
     }
+    
+    public init(heading: Int, lat: Double, lng: Double, speed: Double, orderStatus: String?, orderId: Int?, online: Bool?, statusTime: String?) {
+        self.heading = heading
+        self.lat = lat
+        self.lng = lng
+        self.speed = speed
+        self.orderStatus = orderStatus
+        self.orderId = orderId
+        self.online = online
+        self.statusTime = statusTime
+    }
 }
 
-struct OrderCheck: Encodable, Sendable {
-    let coverPrice: Int
-    let accountCalculationStatus: Bool
-    let distance: Double
-    let duration: String
-    let fullDistance: String
-    let fullDuration: String
-    let inCity: DNetLocationZone
-    let isWaitingTime: Bool
-    let outCity: DNetLocationZone
-    let remainderPrice: Int
-    let startPrice: Int
-    let totalMinutePrice: Int
-    let totalPrice: Int
-    let tripPrice: Int
-    let waitPrice: Int
-    let waitTime: String
+public struct OrderCheck: Encodable, Sendable {
+    public let coverPrice: Int
+    public let accountCalculationStatus: Bool
+    public let distance: Double
+    public let duration: String
+    public let fullDistance: String
+    public let fullDuration: String
+    public let inCity: LocationZone
+    public let isWaitingTime: Bool
+    public let outCity: LocationZone
+    public let remainderPrice: Int
+    public let startPrice: Int
+    public let totalMinutePrice: Int
+    public let totalPrice: Int
+    public let tripPrice: Int
+    public let waitPrice: Int
+    public let waitTime: String
     
     enum CodingKeys: String, CodingKey {
         case coverPrice = "cover_price"
@@ -80,6 +91,25 @@ struct OrderCheck: Encodable, Sendable {
         case waitPrice = "wait_price"
         case waitTime = "wait_time"
     }
+    
+    public init(coverPrice: Int, accountCalculationStatus: Bool, distance: Double, duration: String, fullDistance: String, fullDuration: String, inCity: LocationZone, isWaitingTime: Bool, outCity: LocationZone, remainderPrice: Int, startPrice: Int, totalMinutePrice: Int, totalPrice: Int, tripPrice: Int, waitPrice: Int, waitTime: String) {
+        self.coverPrice = coverPrice
+        self.accountCalculationStatus = accountCalculationStatus
+        self.distance = distance
+        self.duration = duration
+        self.fullDistance = fullDistance
+        self.fullDuration = fullDuration
+        self.inCity = inCity
+        self.isWaitingTime = isWaitingTime
+        self.outCity = outCity
+        self.remainderPrice = remainderPrice
+        self.startPrice = startPrice
+        self.totalMinutePrice = totalMinutePrice
+        self.totalPrice = totalPrice
+        self.tripPrice = tripPrice
+        self.waitPrice = waitPrice
+        self.waitTime = waitTime
+    }
 }
 
 struct DNetLocationZone: Encodable {
@@ -87,5 +117,24 @@ struct DNetLocationZone: Encodable {
     let duration: String
     let waitPrice: Double
     let waitTime: String
+}
+
+public struct LocationZone: Encodable, Sendable {
+    public let distance: Double
+    public let duration: String
+    public let waitPrice: Double
+    public let waitTime: String
     
+    public init(distance: Double, duration: String, waitPrice: Double, waitTime: String) {
+        self.distance = distance
+        self.duration = duration
+        self.waitPrice = waitPrice
+        self.waitTime = waitTime
+    }
+}
+
+extension LocationZone {
+    var asReq: DNetLocationZone {
+        .init(distance: distance, duration: duration, waitPrice: waitPrice, waitTime: waitTime)
+    }
 }
--- Tests/IldamSDKTests/DriverRequestsTest/DetailTest/DriverSendCoordTest.swift
+++ Tests/IldamSDKTests/DriverRequestsTest/DetailTest/DriverSendCoordTest.swift
@@ -17,7 +17,7 @@ final class DriverSendCoordTest: XCTestCase {
 
         UserSettings.shared.accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI1IiwianRpIjoiYjRlM2VkMDU5YmJiNDMyM2FjZmE0YzgzZDk5OWQ0YTlhNDMwNWI3MWIwMThmNDZhOWJhYTgzMTI0ZDg0MzhlNjYxNjA4NjE4MDAwNmNjODciLCJpYXQiOjE3NDk0NDc0MjIuMTIwOTksIm5iZiI6MTc0OTQ0NzQyMi4xMjA5OTIsImV4cCI6MTc4MDk4MzQyMi4xMTY2NDUsInN1YiI6IjU1MiIsInNjb3BlcyI6WyJleGVjdXRvciJdfQ.kkwUmrbSnEY7raLa2uzF_gzwrOM7z1uHwr13s6Bu2qjgAn7NoRRhYIJcJX0PzSuwX-fxkKUizgPu_T5Nrstuyuk5x8jLmlOxuCYjklRVRXkWLorXR7pnStgimYlsZ6UXgnE1CdVNT4U-93UGSpGye9mouYpGRfMrIgQh8xB3GSIb1mvDFrffmUYK3y4YmFb1Oi4Q9O7b_fXpaA31PnOxHtqMRVL5ChFAJ7HlHkpT0q5qrAwwxMXM-dRWEQwfkwbfrcC45_clvJb-mKF0s3RlTyCL26Z3WOY1OOd-5wcuhA-sWAGmh5VbP25YwwlWAA7PINEn0vsCUDFFCo32bsmItY28HhdbpQr9wYDIlYYYBz4gGkxaPkRfUJWluWTZ_ZM6zFjSsDCLpGUmzGemIEm-4s49a6Kzu77iEALDVYP9QN33lShrWzGDZwTvunYTKAP9DGfU7wJeq0kUfkmNsBMQz1rMJLVmYcN0Cmoem35CodirujR1Z4vrW7-7Jvk0fC3K09io_oPcZJviXpGVHqux7AJvtXFcrA0rWuOHFXSkpMcujJEG7PKY6xdbRnp8xTlPbMubJu_D7bpMPaEkVeaLcu9y7ysAPv_QdLchD07C936O8HliD8zTnF8zWrfm3oZYzEQiK4JYez_qOlJiJBG6F1UoystxwEmbl1RQihWTsPg"
         
-        let result = try? await gateway.sendCoordsWithoutOrder(lat: 0, lng: 0, heading: 0, speed: 0)
+        let result = try? await gateway.sendCoords(lat: 0, lng: 0, heading: 0, speed: 0)
         
 //        let resultOrder = try? await gateway.sendCoordsWithOrder(
 //            body: SendCoordsBody(
